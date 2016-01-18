f671a89e6f7c0f851ccf9d8ce6f76c0aa6824f07
Maryaeria <maryamsyed2096@gmail.com>
2016-01-18 13:46:44 -0800

    Complete level02 (no video exploration available)

---
+++ challenges02/app.js
@@ -0,0 +1,18 @@
+var events = require('events');
+var EventEmitter = events.EventEmitter;
+
+var chat = new EventEmitter();
+var users = [], chatlog = [];
+
+chat.on('message', function(message) {
+  chatlog.push(message);
+});
+
+chat.on('join', function(nickname) {
+  users.push(nickname);
+});
+
+// Emit events here
+chat.emit('join', 'Melinda');
+chat.emit('message', 'I like to move it move it...');
+
