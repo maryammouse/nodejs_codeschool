e424ab603b1c68202acc2038945033aa0d30f8e3
Maryaeria <maryamsyed2096@gmail.com>
2016-01-19 10:50:32 -0800

    Start level06 work with socket.io

---
+++ vid06/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('messages', function(data) {
+        var nickname = client.nickname;
+        client.broadcast.emit("message", nickame + ": " + message);
+
+        client.emit("messages", nickname  + ": " + message);
+    });
+
+    client.on('join', function(name) {
+        client.nickname = name;
+    })
+
+    client.emit('messages', {hello: 'world'});
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8080);
